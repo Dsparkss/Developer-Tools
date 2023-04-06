75e41108d53cd2422bf7f9ebe06de9159286d2fc
Dinesh Babu <125888281+Dsparkss@users.noreply.github.com>
2023-04-06 13:13:33 +0530

    Add files via upload

---
+++ Java/src/Interface/Main.java
@@ -0,0 +1,8 @@
+package Interface;
+
+//Basically contains  abstract function no body of the function is not allowed
+//variables are static and final
+//wat the class is doing and not how its doing
+public class Main {
+
+}
